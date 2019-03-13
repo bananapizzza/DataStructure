cdbeea5ea4d9e775a874334f8b032c776397025e
bananapizzza <33973926+bananapizzza@users.noreply.github.com>
2019-03-13 13:14:00 -0700

    For pull request (#2)
    * Revised getSumUsingIterate: Added carry condition check / Added getSumUsingRecursive / Revised toString: Added getSize & Deleted int size.
    Test methods added: test_getSumUsingRecursive_sameLength, test_getSumUsingRecursive_sameLengthWithCarry, test_getSumUsingRecursive_differentLength.
    
    * Added Node constructor with data. Added class Storage. Added insertBefore, LPadList, getSumBackward, addList methods.
    Added test_getSumBackward, test_getSumBackwardWithCarry test methods.
    
    * Revised error in LPadList method. Added getIntersection method.
    Added test_getIntersectionWithIntersection, test_getIntersectionWithoutIntersection methods.
    
    * Revised as the comments of xaveng

--- src/com/minseung/datastructure/LinkedListNode.java
+++ src/com/minseung/datastructure/LinkedListNode.java
@@ -2,11 +2,17 @@ package com.minseung.datastructure;
 
 class LinkedList {
     Node header;
-    int size = 0;
 
     static class Node {
         int data;
         Node next = null;
+
+        Node() {
+        }
+
+        Node(int data) {
+            this.data = data;
+        }
     }
 
 
@@ -23,7 +29,6 @@ class LinkedList {
             n = n.next;
         }
         n.next = end;
-        size++;
     }
 
     void delete(int d) {
@@ -37,23 +42,15 @@ class LinkedList {
         }
     }
 
-    void printData(LinkedList ll) {
-        String s = ll.toString();
+    void printData() {
+        String s = this.toString();
         System.out.println(s);
     }
-//    void retrieve() {
-//        Node n = header.next;
-//        while (n.next != null) {
-//            System.out.print(n.data + " -> ");
-//            n = n.next;
-//        }
-//        System.out.println(n.data);
-//    }
 
     @Override
     public String toString() {
         String s = "";
-        if (size == 0)
+        if (getSize() == 0)
             return s;
         Node n = header.next;
         while (n.next != null) {
@@ -62,6 +59,16 @@ class LinkedList {
         }
         s = s + Integer.toString(n.data);
         return s;
+    }
+
+    int getSize() {
+        Node n = this.header;
+        int size = 0;
+        while (n.next != null) {
+            size++;
+            n = n.next;
+        }
+        return size;
     }
 
     void removeDups() {
@@ -79,70 +86,9 @@ class LinkedList {
         }
     }
 
-    Node getFirst() {
+    Node getHeader() {
         return header;
     }
-
-    //Solution 1: Basic
-     /*
-    Node findKthNode(Node first, int k){
-    Node n = first;
-    int count = 0;
-    while(n.next!=null){
-      n = n.next;
-      count++;
-    }
-
-    n = first;
-    for(int i = 0; i < count - k + 1; i++){
-      n = n.next;
-    }
-    return n;
-  }
-  */
-
-    //Solution 2: Using recursive function
-  /*
-  static class Reference{
-    public int count = 0;
-  }
-  Node findKthNode(Node n, int k, Reference r){
-    if(n == null)
-      return null;
-    Node found = findKthNode(n.next, k, r);
-    r.count++;
-    if(r.count == k){
-      return n;
-    }
-    return found;
-  }
-  */
-    /*
-    //Solution 3: Using pointer
-    Node findKthNode(Node first, int k) {
-        Node p1 = first;
-        Node p2 = first;
-
-        for (int i = 0; i < k; i++) {
-            p1 = p1.next;
-        }
-
-        while (p1 != null) {
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-        return p2;
-    }
-
-    boolean deleteNode(Node n) {
-        if (n == null || n.next == null)
-            return false;
-        Node next = n.next;
-        n.data = next.data;
-        n.next = next.next;
-        return true;
-    }
-    */
 
     Node getNode(int k) {
         Node n = header;
@@ -213,6 +159,11 @@ class LinkedList {
 
 }
 
+class Storage {
+    int carry = 0;
+    LinkedList.Node result = null;
+}
+
 
 public class LinkedListNode {
     public static void main(String[] args) {
@@ -225,37 +176,36 @@ public class LinkedListNode {
         ll.append(3);
         ll.append(4);
         ll.append(5);
-        ll.printData(ll);
-        //found = ll.findKthNode(ll.getFirst(), k, r);
-        found = ll.findKthNodeBasic(ll.getFirst(), k);
+        ll.printData();
+        found = ll.findKthNodeBasic(ll.getHeader(), k);
         System.out.println(found.data);
 
-        found = ll.findKthNodeUsingRecursive(ll.getFirst(), k, r);
+        found = ll.findKthNodeUsingRecursive(ll.getHeader(), k, r);
         System.out.println(found.data);
 
-        found = ll.findKthNodeUsingPointer(ll.getFirst(), k);
+        found = ll.findKthNodeUsingPointer(ll.getHeader(), k);
         System.out.println(found.data);
         ll.deleteMiddleNode(ll.getNode(k));
-        ll.printData(ll);
-        /*
+        ll.printData();
+
         LinkedList l1 = new LinkedList();
         LinkedList l2 = new LinkedList();
         l1.append(9);
         l1.append(1);
         l1.append(4);
-        l1.retrieve();
+        l1.printData();
         l2.append(6);
         l2.append(4);
-        l2.append(3);
-        l2.retrieve();
+        l2.append(9);
+        l2.printData();
         LinkedListNode linkedListNode = new LinkedListNode();
-        LinkedList l3 = linkedListNode.getSum(l1, l2);
-        l3.retrieve();
-        */
+        LinkedList l3 = linkedListNode.getSumUsingIterate(l1, l2);
+        l3.printData();
+
 
     }
 
-    public LinkedList getSum(LinkedList l1, LinkedList l2) {
+    public LinkedList getSumUsingIterate(LinkedList l1, LinkedList l2) {
         int carry = 0;
         LinkedList l3 = new LinkedList();
         LinkedList.Node n1 = l1.header.next;
@@ -279,6 +229,135 @@ public class LinkedListNode {
             }
             l3.append(result);
         }
+
+        if (carry != 0) {
+            l3.append(carry);
+        }
+
         return l3;
+    }
+
+    public LinkedList.Node getSumUsingRecursive(LinkedList.Node n1, LinkedList.Node n2, int carry) {
+        if (n1 == null && n2 == null && carry == 0)
+            return null;
+
+        LinkedList.Node result = new LinkedList.Node();
+        int value = carry;
+
+        if (n1 != null) {
+            value += n1.data;
+        }
+        if (n2 != null) {
+            value += n2.data;
+        }
+        result.data = value % 10;
+
+        if (n1 != null || n2 != null) {
+            LinkedList.Node next = getSumUsingRecursive(n1 == null ? null : n1.next, n2 == null ? null : n2.next, value >= 10 ? value / 10 : 0);
+            result.next = next;
+        }
+        return result;
+    }
+
+    //TODO: Create test case
+    public LinkedList.Node insertBefore(LinkedList.Node n, int data) {
+        LinkedList.Node n1 = new LinkedList.Node(data);
+        if (n != null)
+            n1.next = n;
+
+        return n1;
+    }
+
+    //TODO: Create test case
+    public LinkedList.Node lPadList(LinkedList.Node current, int size) {
+        LinkedList.Node n = current;
+        for (int i = 0; i < size; i++) {
+            n = insertBefore(n, 0);
+        }
+        return n;
+    }
+
+    public LinkedList.Node getSumBackward(LinkedList l1, LinkedList l2) {
+
+        int size1 = l1.getSize();
+        int size2 = l2.getSize();
+
+        if (size1 < size2) {
+            l1.header.next = lPadList(l1.header.next, size2 - size1);
+        }
+        if (size1 > size2) {
+            l2.header.next = lPadList(l2.header.next, size1 - size2);
+        }
+
+        Storage storage = addList(l1.header.next, l2.header.next);
+
+        if (storage.carry != 0) {
+            storage.result = insertBefore(storage.result, storage.carry);
+        }
+        return storage.result;
+    }
+
+    public Storage addList(LinkedList.Node n1, LinkedList.Node n2) {
+        if (n1 == null && n2 == null) {
+            Storage storage = new Storage();
+            return storage;
+        }
+
+        Storage storage = addList(n1.next, n2.next);
+        int value = storage.carry;
+        value += n1.data;
+        value += n2.data;
+
+        storage.result = insertBefore(storage.result, value % 10);
+        storage.carry = value / 10;
+
+        return storage;
+    }
+
+    //Find intersection node of two different linked lists and return the node
+    public LinkedList.Node getIntersection(LinkedList l1, LinkedList l2) {
+        int size1 = l1.getSize();
+        int size2 = l2.getSize();
+        LinkedList.Node n1 = l1.header.next;
+        LinkedList.Node n2 = l2.header.next;
+
+        if (size1 < size2) {
+            n2 = l2.getNode(size2 - size1 + 1);
+        } else if (size1 > size2) {
+            n1 = l1.getNode(size1 - size2 + 1);
+        }
+
+        while (n1 != null && n2 != null) {
+            if (n1 == n2) {
+                return n1;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return null;
+    }
+
+    public LinkedList.Node getIntersectionUsingRecursive(LinkedList l1, LinkedList l2){
+        int size1 = l1.getSize();
+        int size2 = l2.getSize();
+        if(size1 < size2){
+            l1.header.next = lPadList(l1.header.next, size2 - size1);
+        }
+        if(size1 > size2){
+            l2.header.next = lPadList(l2.header.next, size1 - size2);
+        }
+        LinkedList.Node found = getIntersectionNode(l1.header.next, l2.header.next);
+        return found;
+    }
+
+    public LinkedList.Node getIntersectionNode(LinkedList.Node n1, LinkedList.Node n2){
+        if(n1 == null && n2 == null){
+            return null;
+        }
+        LinkedList.Node n = getIntersectionNode(n1 == null? null : n1.next, n2 == null? null : n2.next);
+        if(n1 != null && n2 != null && n1 == n2){
+            return n1;
+        }
+        return n;
     }
 }
--- test/com/minseung/datastructure/LinkedListTest.java
+++ test/com/minseung/datastructure/LinkedListTest.java
@@ -6,6 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
 
+    LinkedList makeLinkedList(int[] data){
+        LinkedList ll = new LinkedList();
+        for(int i = 0; i < data.length; i++){
+            ll.append(data[i]);
+        }
+        return ll;
+    }
+
+    @Test
+    void test_makeLinkedList(){
+        LinkedList l1 = new LinkedList();
+        LinkedList l2;
+        LinkedList.Node n1 = new LinkedList.Node();
+        n1.data = 1;
+        l1.header.next = n1;
+        LinkedList.Node n2 = new LinkedList.Node();
+        n2.data = 7;
+        n1.next = n2;
+        LinkedList.Node n3 = new LinkedList.Node();
+        n3.data = 5;
+        n2.next = n3;
+
+        int[] data = {1,7,5};
+        l2 = makeLinkedList(data);
+
+        assertEquals(l1.toString(), l2.toString());
+    }
+
     @Test
     void test_toString_emptyLL() {
         LinkedList l1 = new LinkedList();
@@ -17,7 +45,6 @@ class LinkedListTest {
         LinkedList l1 = new LinkedList();
         l1.header.next = new LinkedList.Node();
         l1.header.next.data = 1;
-        l1.size++;
         assertEquals("1", l1.toString());
     }
 
@@ -27,11 +54,9 @@ class LinkedListTest {
         LinkedList.Node n1 = new LinkedList.Node();
         n1.data = 1;
         l1.header.next = n1;
-        l1.size++;
         LinkedList.Node n2 = new LinkedList.Node();
         n2.data = 2;
         n1.next = n2;
-        l1.size++;
 
         assertEquals("1 -> 2", l1.toString());
     }
@@ -43,15 +68,12 @@ class LinkedListTest {
         LinkedList.Node n1 = new LinkedList.Node();
         n1.data = 1;
         l1.header.next = n1;
-        l1.size++;
         LinkedList.Node n2 = new LinkedList.Node();
         n2.data = 7;
         n1.next = n2;
-        l1.size++;
         LinkedList.Node n3 = new LinkedList.Node();
         n3.data = 5;
         n2.next = n3;
-        l1.size++;
 
         l2.append(1);
         l2.append(7);
@@ -62,18 +84,11 @@ class LinkedListTest {
 
     @Test
     void test_delete() {
-        LinkedList l1 = new LinkedList();
-        LinkedList l2 = new LinkedList();
-        l1.append(1);
-        l1.append(3);
-        l1.append(4);
-        l1.append(5);
+        int[] data1 = {1,3,4,5};
+        int[] data2 = {1,2,3,4,5};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
 
-        l2.append(1);
-        l2.append(2);
-        l2.append(3);
-        l2.append(4);
-        l2.append(5);
         l2.delete(2);
 
         assertEquals(l1.toString(), l2.toString());
@@ -81,17 +96,11 @@ class LinkedListTest {
 
     @Test
     void test_removeDups() {
-        LinkedList l1 = new LinkedList();
-        LinkedList l2 = new LinkedList();
-        l1.append(2);
-        l1.append(3);
-        l1.append(4);
+        int[] data1 = {2,3,4};
+        int[] data2 = {2,3,2,4,4};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
 
-        l2.append(2);
-        l2.append(3);
-        l2.append(2);
-        l2.append(4);
-        l2.append(4);
         l2.removeDups();
 
         assertEquals(l1.toString(), l2.toString());
@@ -103,31 +112,23 @@ class LinkedListTest {
         LinkedList.Node n1 = new LinkedList.Node();
         n1.data = 1;
         l1.header.next = n1;
-        l1.size++;
         LinkedList.Node n2 = new LinkedList.Node();
         n2.data = 7;
         n1.next = n2;
-        l1.size++;
         LinkedList.Node n3 = new LinkedList.Node();
         n3.data = 5;
         n2.next = n3;
-        l1.size++;
 
         assertEquals(n3, l1.getNode(3));
     }
 
     @Test
     void test_deleteMiddleNode() {
-        LinkedList l1 = new LinkedList();
-        LinkedList l2 = new LinkedList();
-        l1.append(2);
-        l1.append(3);
-        l1.append(4);
+        int[] data1 = {2,3,4};
+        int[] data2 = {2,3,2,4};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
 
-        l2.append(2);
-        l2.append(3);
-        l2.append(2);
-        l2.append(4);
         l2.deleteMiddleNode(l2.getNode(3));
 
         assertEquals(l1.toString(), l2.toString());
@@ -135,88 +136,181 @@ class LinkedListTest {
 
     @Test
     void test_findKthNodeBasic() {
-        LinkedList l1 = new LinkedList();
-        LinkedList.Node n1 = new LinkedList.Node();
-        n1.data = 1;
-        l1.header.next = n1;
-        l1.size++;
-        LinkedList.Node n2 = new LinkedList.Node();
-        n2.data = 7;
-        n1.next = n2;
-        l1.size++;
-        LinkedList.Node n3 = new LinkedList.Node();
-        n3.data = 5;
-        n2.next = n3;
-        l1.size++;
+        int[] data = {1,7,5};
+        LinkedList l1 = makeLinkedList(data);
 
-        assertEquals(n3, l1.findKthNodeBasic(l1.header, 1));
+        assertEquals(l1.getNode(3), l1.findKthNodeBasic(l1.header, 1));
     }
 
     @Test
-    void test_findKthNodeUsingRecursive(){
-        LinkedList l1 = new LinkedList();
-        LinkedList.Node n1 = new LinkedList.Node();
-        n1.data = 1;
-        l1.header.next = n1;
-        l1.size++;
-        LinkedList.Node n2 = new LinkedList.Node();
-        n2.data = 7;
-        n1.next = n2;
-        l1.size++;
-        LinkedList.Node n3 = new LinkedList.Node();
-        n3.data = 5;
-        n2.next = n3;
-        l1.size++;
+    void test_findKthNodeUsingRecursive() {
+        int[] data = {1,7,5};
+        LinkedList l1 = makeLinkedList(data);
 
         LinkedList.Reference r = new LinkedList.Reference();
 
-        assertEquals(n3, l1.findKthNodeUsingRecursive(l1.header, 1, r));
+        assertEquals(l1.getNode(3), l1.findKthNodeUsingRecursive(l1.header, 1, r));
     }
 
     @Test
-    void test_findKthNodeUsingPointer(){
-        LinkedList l1 = new LinkedList();
-        LinkedList.Node n1 = new LinkedList.Node();
-        n1.data = 1;
-        l1.header.next = n1;
-        l1.size++;
-        LinkedList.Node n2 = new LinkedList.Node();
-        n2.data = 7;
-        n1.next = n2;
-        l1.size++;
-        LinkedList.Node n3 = new LinkedList.Node();
-        n3.data = 5;
-        n2.next = n3;
-        l1.size++;
+    void test_findKthNodeUsingPointer() {
+        int[] data = {1,7,5};
+        LinkedList l1 = makeLinkedList(data);
 
-        assertEquals(n3, l1.findKthNodeUsingPointer(l1.header, 1));
+        assertEquals(l1.getNode(3), l1.findKthNodeUsingPointer(l1.header, 1));
     }
 
     @Test
-    void test_getSum(){
-        LinkedList l1 = new LinkedList();
-        LinkedList l2 = new LinkedList();
-        l1.append(9);
-        l1.append(1);
-        l1.append(4);
-
-        l2.append(7);
-        l2.append(6);
-        l2.append(4);
-        l2.append(3);
+    void test_getSumUsingIterate() {
+        int[] data1 = {9,1,4};
+        int[] data2 = {7,6,4,3};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
 
         LinkedListNode linkedListNode = new LinkedListNode();
-        LinkedList l3 = linkedListNode.getSum(l1, l2);
+        LinkedList actual = linkedListNode.getSumUsingIterate(l1, l2);
 
-        LinkedList l4 = new LinkedList();
-        l4.append(6);
-        l4.append(8);
-        l4.append(8);
-        l4.append(3);
-//        l4.append(5);
-//        l4.append(6);
-//        l4.append(7);
+        int[] data3 = {6,8,8,3};
+        LinkedList expected = makeLinkedList(data3);
 
-        assertEquals(l4.toString(), l3.toString());
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void test_getSumUsingRecursive_sameLength() {
+
+        int[] data1 = {9,1,4};
+        int[] data2 = {7,6,4};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getSumUsingRecursive(l1.getNode(1), l2.getNode(1), 0);
+        LinkedList actual = new LinkedList();
+        actual.header.next = result;
+
+        int[] data3 = {6,8,8};
+        LinkedList expected = makeLinkedList(data3);
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void test_getSumUsingRecursive_sameLengthWithCarry() {
+        int[] data1 = {9,1,4};
+        int[] data2 = {7,6,9};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getSumUsingRecursive(l1.getNode(1), l2.getNode(1), 0);
+        LinkedList actual = new LinkedList();
+        actual.header.next = result;
+
+        int[] data3 = {6,8,3,1};
+        LinkedList expected = makeLinkedList(data3);
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void test_getSumUsingRecursive_differentLength() {
+        int[] data1 = {9,1,4};
+        int[] data2 = {7,6,4,3};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getSumUsingRecursive(l1.getNode(1), l2.getNode(1), 0);
+        LinkedList actual = new LinkedList();
+        actual.header.next = result;
+
+        int[] data3 = {6,8,8,3};
+        LinkedList expected = makeLinkedList(data3);
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void test_getSumBackward() {
+        int[] data1 = {9,1,4};
+        int[] data2 = {7,6,4,3};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getSumBackward(l1, l2);
+        LinkedList actual = new LinkedList();
+        actual.header.next = result;
+
+        int[] data3 = {8,5,5,7};
+        LinkedList expected = makeLinkedList(data3);
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void test_getSumBackwardWithCarry() {
+        int[] data1 = {8,7,4};
+        int[] data2 = {6,8,3};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getSumBackward(l1, l2);
+        LinkedList actual = new LinkedList();
+        actual.header.next = result;
+
+        int[] data3 = {1,5,5,7};
+        LinkedList expected = makeLinkedList(data3);
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void test_getIntersectionWithIntersection() {
+        int[] data1 = {5,7,9,10,7,6};
+        int[] data2 = {6,8};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedList.Node connect = l2.getNode(2);
+        LinkedList.Node intersection = l1.getNode(4);
+        connect.next = intersection;
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getIntersection(l1, l2);
+
+        assertEquals(intersection, result);
+    }
+
+    @Test
+    void test_getIntersectionWithoutIntersection() {
+        int[] data1 = {5,7,9,10,7,6};
+        int[] data2 = {6,8,10,3};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getIntersection(l1, l2);
+
+        assertEquals(null, result);
+    }
+
+    @Test
+    void test_getIntersectionUsingRecursive() {
+        int[] data1 = {5,7,9,10,7,6};
+        int[] data2 = {6,8};
+        LinkedList l1 = makeLinkedList(data1);
+        LinkedList l2 = makeLinkedList(data2);
+
+        LinkedList.Node connect = l2.getNode(2);
+        LinkedList.Node intersection = l1.getNode(4);
+        connect.next = intersection;
+
+        LinkedListNode linkedListNode = new LinkedListNode();
+        LinkedList.Node result = linkedListNode.getIntersectionUsingRecursive(l1, l2);
+
+        assertEquals(intersection, result);
     }
 }
