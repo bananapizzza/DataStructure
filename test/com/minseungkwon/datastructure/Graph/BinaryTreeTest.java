1c591d0786dbeb0054d274edb7e0c99bf3f5c762
Minseung Kwon <bananapizzza@gmail.com>
2019-04-24 12:44:48 -0700

    Made makeArrayForTree method. Added one more true test case for the methods using recursive. (Since those two methods uses different logic from the one using object. True case 2 is true for the two methods using recursive, but is false for the method using object.) Changed from AssertEquals to AssertTrue & AssertFalse.

--- test/com/minseungkwon/datastructure/Graph/BinaryTreeTest.java
+++ test/com/minseungkwon/datastructure/Graph/BinaryTreeTest.java
@@ -1,11 +1,19 @@
 package com.minseungkwon.datastructure.Graph;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 
 public class BinaryTreeTest {
+    public Integer[] makeArrayForTree() {
+        Integer[] arr = new Integer[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
     @Test
     public void test_inorder() {
         BinaryTree<Integer> bt = new BinaryTree();
@@ -62,10 +70,7 @@ public class BinaryTreeTest {
 
     @Test
     public void test_makeTree() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
+        Integer[] arr = makeArrayForTree();
         BinaryTree<Integer> bt = new BinaryTree<>();
         bt.makeTree(arr);
         assertEquals("4102375689", bt.toString());
@@ -73,10 +78,7 @@ public class BinaryTreeTest {
 
     @Test
     public void test_searchBTreeWithExistentData() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
+        Integer[] arr = makeArrayForTree();
         BinaryTree<Integer> bt = new BinaryTree<>();
         bt.makeTree(arr);
         assertEquals(true, bt.searchBTree(2));
@@ -84,21 +86,15 @@ public class BinaryTreeTest {
 
     @Test
     public void test_searchBTreeWithoutExistentData() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
+        Integer[] arr = makeArrayForTree();
         BinaryTree<Integer> bt = new BinaryTree<>();
         bt.makeTree(arr);
-        assertEquals(false, bt.searchBTree(12));
+        assertFalse(bt.searchBTree(12));
     }
 
     @Test
     public void test_BSTtoListRecursive() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
+        Integer[] arr = makeArrayForTree();
         BinaryTree<Integer> bt = new BinaryTree<>();
         bt.makeTree(arr);
 
@@ -107,10 +103,7 @@ public class BinaryTreeTest {
 
     @Test
     public void test_BSTtoList() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
+        Integer[] arr = makeArrayForTree();
         BinaryTree<Integer> bt = new BinaryTree<>();
         bt.makeTree(arr);
 
@@ -118,78 +111,82 @@ public class BinaryTreeTest {
     }
 
     @Test
-    public void test_isBalancedUsingTwoRecursiveTrueCase() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        BinaryTree<Integer> bt = new BinaryTree<>();
-        bt.makeTree(arr);
-
-        assertEquals(true, bt.isBalancedUsingTwoRecursive());
-    }
-
-    @Test
-    public void test_isBalancedUsingTwoRecursiveFalseCase() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        BinaryTree<Integer> bt = new BinaryTree<>();
-        bt.makeTree(arr);
-        bt.getRoot().right.right.right.right = new Node(10);
-
-        assertEquals(false, bt.isBalancedUsingTwoRecursive());
-    }
-
-    @Test
-    public void test_isBalancedUsingOneRecursiveTrueCase() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        BinaryTree<Integer> bt = new BinaryTree<>();
-        bt.makeTree(arr);
-
-        assertEquals(true, bt.isBalancedUsingOneRecursive());
-    }
-
-    @Test
-    public void test_isBalancedUsingOneRecursiveFalseCase() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        BinaryTree<Integer> bt = new BinaryTree<>();
-        bt.makeTree(arr);
-        bt.getRoot().right.right.right.right = new Node(10);
-
-        assertEquals(false, bt.isBalancedUsingOneRecursive());
-    }
-
-    @Test
-    public void test_isBalancedUsingObjectTrueCase() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        BinaryTree<Integer> bt = new BinaryTree<>();
-        bt.makeTree(arr);
-
-        assertEquals(true, bt.isBalancedUsingObject());
-    }
-
-    @Test
-    public void test_isBalancedUsingObjectFalseCase() {
-        Integer[] arr = new Integer[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
+    public void test_isBalancedUsingTwoRecursiveTrueCase1() {
+        Integer[] arr = makeArrayForTree();
         BinaryTree<Integer> bt = new BinaryTree<>();
         bt.makeTree(arr);
         bt.getRoot().right.right.right.right = new Node(10);
         bt.getRoot().right.right.left = new Node(11);
 
-        assertEquals(false, bt.isBalancedUsingObject());
+        assertTrue(bt.isBalancedUsingTwoRecursive());
+    }
+
+    @Test
+    public void test_isBalancedUsingTwoRecursiveTrueCase2() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+
+        assertTrue(bt.isBalancedUsingTwoRecursive());
+    }
+
+    @Test
+    public void test_isBalancedUsingTwoRecursiveFalseCase() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+        bt.getRoot().right.right.right.right = new Node(10);
+
+        assertFalse(bt.isBalancedUsingTwoRecursive());
+    }
+
+    @Test
+    public void test_isBalancedUsingOneRecursiveTrueCase1() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+
+        assertTrue(bt.isBalancedUsingOneRecursive());
+    }
+
+    @Test
+    public void test_isBalancedUsingOneRecursiveTrueCase2() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+        bt.getRoot().right.right.right.right = new Node(10);
+        bt.getRoot().right.right.left = new Node(11);
+
+        assertTrue(bt.isBalancedUsingOneRecursive());
+    }
+
+    @Test
+    public void test_isBalancedUsingOneRecursiveFalseCase() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+        bt.getRoot().right.right.right.right = new Node(10);
+
+        assertFalse(bt.isBalancedUsingOneRecursive());
+    }
+
+    @Test
+    public void test_isBalancedUsingObjectTrueCase() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+
+        assertTrue(bt.isBalancedUsingObject());
+    }
+
+    @Test
+    public void test_isBalancedUsingObjectFalseCase() {
+        Integer[] arr = makeArrayForTree();
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.makeTree(arr);
+        bt.getRoot().right.right.right.right = new Node(10);
+        bt.getRoot().right.right.left = new Node(11);
+
+        assertFalse(bt.isBalancedUsingObject());
     }
 }
