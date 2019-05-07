5e8849c250f954ee1e7b8aa9bceefaa11d82ff3c
Minseung Kwon <bananapizzza@gmail.com>
2019-05-07 14:57:30 -0700

    Added StringBuilder class and test cases.

---
+++ src/com/minseungkwon/datastructure/StringBuilder.java
@@ -0,0 +1,39 @@
+package com.minseungkwon.datastructure;
+
+public class StringBuilder {
+    int index;
+    int size;
+    char[] value;
+
+    StringBuilder() {
+        index = 0;
+        size = 0;
+        value = new char[size];
+    }
+
+    public void append(String str) {
+        if (str == null) {
+            return;
+        }
+        ensureCapacity(str);
+        for (int i = 0; i < str.length(); i++) {
+            value[index++] = str.charAt(i);
+        }
+    }
+
+    private void ensureCapacity(String str) {
+        if ((str.length() + index) > size) {
+            size = (str.length() + size) * 2;
+
+            char[] newValue = new char[size];
+            for (int i = 0; i < index; i++) {
+                newValue[i] = value[i];
+            }
+            value = newValue;
+        }
+    }
+
+    public String toString() {
+        return new String(value, 0, index);
+    }
+}
---
+++ test/com/minseungkwon/datastructure/StringBuilderTest.java
@@ -0,0 +1,19 @@
+package com.minseungkwon.datastructure;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringBuilderTest {
+    @Test
+    public void test_append(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("sung");
+        sb.append(" is");
+        sb.append(" pretty");
+
+        String s = "sung is pretty";
+
+        assertEquals(s, sb.toString());
+    }
+}
