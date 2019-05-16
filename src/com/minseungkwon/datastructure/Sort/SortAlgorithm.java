24cc71cd18ba32242f4fa208f874df958416a089
Minseung Kwon <bananapizzza@gmail.com>
2019-05-16 12:05:31 -0700

    Added mergeSort and related methods. Added a new test case.

--- src/com/minseungkwon/datastructure/Sort/SortAlgorithm.java
+++ src/com/minseungkwon/datastructure/Sort/SortAlgorithm.java
@@ -41,4 +41,48 @@ public class SortAlgorithm {
         arr[start] = arr[end];
         arr[end] = temp;
     }
+
+    public static void mergeSort(int[] arr) {
+        int[] temp = new int[arr.length];
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        mergeSort(arr, temp, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] arr, int[] temp, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, temp, start, mid);
+            mergeSort(arr, temp, mid + 1, end);
+            merge(arr, temp, start, mid, end);
+        }
+    }
+
+    private static void merge(int[] arr, int[] temp, int start, int mid, int end) {
+        for (int i = start; i <= end; i++) {
+            temp[i] = arr[i];
+        }
+        int index = start;
+        int part1 = start;
+        int part2 = mid + 1;
+        while (part1 <= mid && part2 <= end) {
+            if (temp[part1] <= temp[part2]) {
+                arr[index] = temp[part1];
+                part1++;
+            } else {
+                arr[index] = temp[part2];
+                part2++;
+            }
+            index++;
+        }
+        for (int i = part1; i <= mid; i++) {
+            arr[index++] = temp[i];
+        }
+
+        for (int i = start; i <= end; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
 }
--- test/com/minseungkwon/datastructure/Sort/SortAlgorithmTest.java
+++ test/com/minseungkwon/datastructure/Sort/SortAlgorithmTest.java
@@ -12,4 +12,12 @@ public class SortAlgorithmTest {
         SortAlgorithm.quickSort(arr);
         assertArrayEquals(expected, arr);
     }
+
+    @Test
+    public void test_mergeSort() {
+        int[] arr = {3, 9, 4, 7, 5, 0, 1, 6, 8, 2};
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        SortAlgorithm.mergeSort(arr);
+        assertArrayEquals(expected, arr);
+    }
 }
